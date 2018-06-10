15c2c443df7dd3d1e64555a72f23c20bae21836b
wtgtybhertgeghgtwtg <wtgtybhertgeghgtwtg@gmail.com>
2018-06-09 22:41:22 -0700

    fix: don't break if a directory without `package.json` matches

--- src/readPkgs.js
+++ src/readPkgs.js
@@ -15,17 +15,17 @@ export default function readPkgs(
   patterns: string | Array<string>,
   options?: ReadPkgsOptions = {},
 ): Promise<Array<{directory: string, pkg: Object}>> {
-  if (
-    typeof patterns !== 'string' &&
-    !(
-      Array.isArray(patterns) &&
-      patterns.every(pattern => typeof pattern === 'string')
-    )
+  if (typeof patterns === 'string') {
+    patterns = [patterns];
+  } else if (
+    !Array.isArray(patterns) ||
+    patterns.some(pattern => typeof pattern !== 'string')
   ) {
     return Promise.reject(
       new Error('"patterns" must be a string or an array of strings.'),
     );
   }
+
   if (!isobject(options)) {
     return Promise.reject(
       new Error('"options" must be a ReadPkgsOptions object or undefined.'),
@@ -41,13 +41,15 @@ export default function readPkgs(
 
   return getStream.array(
     fastGlob
-      .stream(patterns, {cwd, onlyDirectories: true, onlyFiles: false})
+      .stream(patterns.map(pattern => path.join(pattern, 'package.json')), {
+        cwd,
+      })
       .pipe(
         new Transform({
           objectMode: true,
           transform(directory, encoding, callback) {
             readPkg(path.resolve(cwd, directory), {normalize}).then(pkg =>
-              callback(null, {directory, pkg}),
+              callback(null, {directory: path.dirname(directory), pkg}),
             );
           },
         }),
